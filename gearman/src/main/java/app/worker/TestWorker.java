7df4ba516b4ab6b12d411aabc0c87ba0d6f8e03e
Eriton Almeida <eriton@mundilivre.com.br>
2019-03-02 10:52:23 -0300

    Update TestWorker.java

--- gearman/src/main/java/app/worker/TestWorker.java
+++ gearman/src/main/java/app/worker/TestWorker.java
@@ -3,6 +3,10 @@ package app.worker;
 public class TestWorker extends AbstractWorker {
 
     public TestWorker() {
-        super(TestFunction.class);        
+        super(TestFunction.class);
+    }
+
+    public static void main(String[] args) {
+        init(TestWorker.class, args);
     }
 }
