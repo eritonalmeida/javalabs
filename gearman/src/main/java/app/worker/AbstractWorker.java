5a79f3c09db42cdec985d001f6c5efda806d61f2
Eriton Almeida <eriton@mundilivre.com.br>
2019-03-02 10:51:18 -0300

    Update AbstractWorker.java

--- gearman/src/main/java/app/worker/AbstractWorker.java
+++ gearman/src/main/java/app/worker/AbstractWorker.java
@@ -5,38 +5,39 @@ import org.gearman.worker.AbstractGearmanFunction;
 import app.config.GearmanConfig;
 
 public class AbstractWorker implements Runnable {
-    
+
     protected GearmanWorkerImpl worker;
-    
+
     public AbstractWorker(Class<? extends AbstractGearmanFunction> function) {
-        
+
         worker = new GearmanWorkerImpl();
-        
+
         worker.addServer(GearmanConfig.HOST, GearmanConfig.PORT);
-        
+
         worker.registerFunction(function);
     }
-    
+
     @Override
     public void run() {
         worker.work();
     }
-    
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        
-        String[] command = System.getProperty("sun.java.command").split(" ");
-        
-        Class workerClass = Class.forName(command[0]);
-        
+
+    public static void init(Class<? extends AbstractWorker> workerClass, String[] args) {
         int threads = 1;
-        
-        if (args.length == 1) {
-            threads = new Integer(args[0]);
-        }
-                   
-        for (int i = 0; i < threads; i++) {
-            new Thread((AbstractWorker) workerClass.newInstance()).start();
+
+        try {
+
+            if (args.length > 0) {
+                threads = new Integer(args[0]);
+            }
+
+            for (int i = 1; i <= threads; i++) {
+                System.out.println("starting thread " + i);
+                new Thread(workerClass.newInstance()).start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-   
 }
